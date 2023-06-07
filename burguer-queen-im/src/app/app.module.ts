253d5ee2a4daa36c6494ac54dfbff7063659f3ef
IndiraPe <forever_tk_16@hotmail.com>
2023-06-06 21:45:38 -0500

    ADD: vista del login-html/css

--- burguer-queen-im/src/app/app.module.ts
+++ burguer-queen-im/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './views/login/login.component';
 
+import { FormsModule } from '@angular/forms';// <---- Importar FormsModule
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { LoginComponent } from './views/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
