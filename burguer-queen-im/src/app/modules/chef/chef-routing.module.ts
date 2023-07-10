476be4ffe968a7649fd054798961bbe49996d01c
IndiraPe <forever_tk_16@hotmail.com>
2023-07-10 15:09:41 -0400

    ADD: funcionalidad del chef terminado

--- burguer-queen-im/src/app/modules/chef/chef-routing.module.ts
+++ burguer-queen-im/src/app/modules/chef/chef-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChefComponent } from './chef/chef.component';
+import { ChefDeliveredComponent } from './chef-delivered/chef-delivered.component';
 
-const routes: Routes = [{
-  path: '',
-  component: ChefComponent
-}];
+const routes: Routes = [
+  {path: '', component: ChefComponent},
+  {path: 'pending', component: ChefComponent},
+  {path: 'delivered', component: ChefDeliveredComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
